31488d8f95f257c532e05ea5ee61ed0b3914ed02
Samvel Gyolecyan <samvelgyolecyan2005@gmail.com>
2024-07-07 15:29:15 +0400

    HomeWorks

---
+++ src/homework1/ForIfExamples.java
@@ -0,0 +1,54 @@
+package homework1;
+
+public class ForIfExamples {
+
+    public static void main(String[] args) {
+
+        // 1. Տպեք թվերը 1-ից 10 օգտագործելով for ցիկլը:
+
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("i: " + i);
+            }
+
+        System.out.println("--------------");
+
+        // 2. Գտեք թվերի գումարը 1-ից 100 օգտագործելով for:
+
+
+            int sum = 0;
+
+            for (int x = 0; x < 100; x++) {
+                sum = sum + x;
+            }
+            System.out.println("sum: " + sum);
+
+        System.out.println("--------------");
+
+        // 3. Տպեք թվերը 1-ից 50 հակառակ հերթականությամբ օգտագործելով for:
+
+
+            for (int y = 50; y >= 1; y--) {
+                System.out.println("y: " + y);
+            }
+
+        System.out.println("--------------");
+
+        // 4. Տպեք բոլոր կենտ թվերը 1-ից 20, առանց օգտագործելու % օպերատորը:
+
+
+            for (int q = 1; q < 100; q = q + 2) {
+                System.out.println("c: " + q);
+            }
+
+        System.out.println("--------------");
+
+       // 5. Գտեք 1-ից 100 բոլոր զույգ թվերի քանակը, առանց օգտագործելու % օպերատորը:
+
+
+            for (int c = 0; c < 100; c = c + 2) {
+               System.out.println("c: " + c);
+            }
+
+    }
+}
