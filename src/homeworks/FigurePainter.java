1a22b3a99b13be972a27c416fa49e0c2e0e7abc1
Samvel Gyolecyan <samvelgyolecyan2005@gmail.com>
2024-07-27 18:17:42 +0400

    HomeWorks 2

--- src/homeworks/FigurePainter.java
+++ src/homeworks/FigurePainter.java
@@ -14,10 +14,9 @@ public class FigurePainter {
 
         System.out.println("1." + "\n");
 
-        int i, j = 0;
 
-        for (i = 0; i < 5; i++) {
-            for (j = 0; j < i + 1; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*" + " ");
             }
             System.out.println();
@@ -37,10 +36,9 @@ public class FigurePainter {
 
         System.out.println("2." + "\n");
 
-        int a, b = 0;
 
-        for (a = 0; a < 5; a++) {
-            for (b = 0; b < 5 - a; b++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - i; j++) {
                 System.out.print("*" + " ");
             }
             System.out.println();
@@ -59,15 +57,13 @@ public class FigurePainter {
 
         System.out.println("3." + "\n");
 
-        int q, r, t = 0;
 
-
-        for (q = 1; q <= 5; q++) {
-            for (r = 0; r < 5 - q; r++) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < 5 - i; j++) {
                 System.out.print(" ");
             }
 
-            for (t = 0; t < q; t++) {
+            for (int k = 0; k < i; k++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -87,14 +83,13 @@ public class FigurePainter {
 
         System.out.println("4." + "\n");
 
-        int x, y, z = 0;
 
-        for (x = 0; x <= 5; x++) {
-            for (y = 0; y < x + 1; y++) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print(" ");
             }
 
-            for (z = 0; z < 5 - x; z++) {
+            for (int k = 0; k < 5 - i; k++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -118,15 +113,14 @@ public class FigurePainter {
 
         System.out.println("5." + "\n");
 
-        int o, k, l, m, n, g = 0;
 
-        for (o = 1; o <= 5; o++) {
+        for (int i = 1; i <= 5; i++) {
 
-            for (k = 0; k < 5 - o; k++) {
+            for (int j = 0; j < 5 - i; j++) {
                 System.out.print(" ");
             }
 
-            for (l = 0; l < o; l++) {
+            for (int k = 0; k < i; k++) {
                 System.out.print(" *");
             }
 
@@ -134,13 +128,13 @@ public class FigurePainter {
         }
 
 
-        for (g = 1; g <= 5; g++) {
+        for (int i = 1; i <= 5; i++) {
 
-            for (m = 0; m < g + 1; m++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print(" ");
             }
 
-            for (n = 0; n < 5 - g; n++) {
+            for (int k = 0; k < 5 - i; k++) {
                 System.out.print("* ");
             }
 
